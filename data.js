1888198e55c11874ebbc2b7dc83006c80d4d6a50
cikguwilson <cikguwilson@gmail.com>
2026-03-31 17:12:52 +0800

    Added hymn component and Immaculate Mother lyrics

---
+++ data.js
@@ -0,0 +1,13 @@
+// data.js
+export const novenaContent = [
+  {
+    type: "hymn",
+    title: "Immaculate Mother",
+    content: "Immaculate Mother, to you do we plead...",
+  },
+  {
+    type: "prayer",
+    title: "The Preparatory Prayer",
+    content: "O Most Holy Virgin Mary...",
+  },
+];
